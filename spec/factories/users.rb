1b8ff2f75f3a84df7ae5aeb673c3aee3d814e86e
ojyussan <matsuoexpress@gmail.com>
2021-06-11 21:24:34 +0900

    Update users.rb

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                  {"test"}
+    email                 {"test@nenpi.com"}
+    password              {"a11111"}
+    password_confirmation {password}
+  end
+end
