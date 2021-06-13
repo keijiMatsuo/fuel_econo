ed9443503a7f5e6f7efcf3afcc39c6cadd34462e
ojyussan <matsuoexpress@gmail.com>
2021-06-13 20:44:49 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,3 +4,15 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
 end
+
+describe 'ユーザー新規登録' do
+  context '新規登録できる' do
+    it 'name, email, password, password_confirmation が存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+    it 'passwordが6文字以上あれば登録できる' do
+      @user.password = 'abCD12'
+      @user.password_confirmation = 'abCD12'
+      expect(@user).to be_valid
+    end
+  end
